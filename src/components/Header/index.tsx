8a20415806ba088384518ec6d742bd4d11ab20a6
JoaoOliveira0117 <joao.oliveira0117@hotmail.com>
2021-10-16 12:04:26 -0300

    call prismic api for post mapping on index page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <>
+      <div className={styles.headerContainer}>
+        <Link href="/">
+            <a>
+              <img className={styles.headerVector} src="/images/codeVector.svg" alt="logo"/>
+              <h1 className={styles.headerText}>
+                blogatin<span className={styles.dot}>.</span>
+              </h1>
+            </a>
+        </Link>
+      </div>
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
+import Header from '../components/Header/index';
 
 interface Post {
   uid?: string;
@@ -16,7 +19,7 @@ interface Post {
 }
 
 interface PostPagination {
-  next_page: string;
+  next_page?: string; //MUDAR DPS
   results: Post[];
 }
 
@@ -24,13 +27,41 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ results }: PostPagination) {
+  return (
+    <>
+      <main className={commonStyles.contentContainer}>
+        <Header/>
+        {results.map(post => 
+          <>
+            <h1>{post.data.title}</h1>
+            <h2>{post.data.subtitle}</h2>
+            <p>{post.data.author}</p>
+            <p>{post.first_publication_date}</p>
+          </>
+        )}
+      </main>
+    </>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ],{
+    fetch: ['publication.title','publication.content'],
+    pageSize: 20,
+  });
 
-//   // TODO
-// };
+  const results = postsResponse.results.map((post: Post) => {
+    return post
+  })
+
+  return {
+    props:{
+      results
+    },
+    revalidate: 60*60*24,
+  }
+};
